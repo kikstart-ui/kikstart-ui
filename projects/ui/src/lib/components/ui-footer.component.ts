759adf294089555489669df578c1b0dbdf72b3a2
Bram Borggreve <borggreve@gmail.com>
2019-12-02 12:08:21 -0500

    feat: add margin property to ui-footer

--- projects/ui/src/lib/components/ui-footer.component.ts
+++ projects/ui/src/lib/components/ui-footer.component.ts
@@ -4,43 +4,32 @@ import { UiLink } from '../interfaces/ui-link';
 @Component({
   selector: 'ui-footer',
   template: `
-    <footer class="my-5 footer">
+    <footer class="footer my-{{ margin }}">
       <div class="container">
         <ng-container *ngIf="centered">
           <div class="text-center">
-            <ng-container *ngIf="html"><div [innerHTML]="html"></div></ng-container>
-            <ng-container *ngIf="text">{{ text }}</ng-container>
-            <div class="my-3">
-              <ng-container *ngIf="links.length">
-                <ui-link *ngFor="let link of links" [link]="link" linkClass="text-muted"></ui-link>
-              </ng-container>
+            <div *ngIf="html" [innerHTML]="html"></div>
+            <div *ngIf="text">{{ text }}</div>
+            <div class="my-2">
+              <ui-links [links]="links" linkClass="text-muted"></ui-links>
             </div>
           </div>
         </ng-container>
         <ng-container *ngIf="!centered">
           <div class="float-right">
-            <ng-container *ngIf="links.length">
-              <ui-link *ngFor="let link of links" [link]="link" linkClass="text-muted"></ui-link>
-            </ng-container>
+            <ui-links [links]="links" linkClass="text-muted"></ui-links>
           </div>
           <div>
-            <ng-container *ngIf="html"><div [innerHTML]="html"></div></ng-container>
-            <ng-container *ngIf="text">{{ text }}</ng-container>
+            <div *ngIf="html" [innerHTML]="html"></div>
+            <div *ngIf="text">{{ text }}</div>
           </div>
         </ng-container>
       </div>
     </footer>
   `,
-  styles: [
-    `
-      .footer {
-        border-top: 1px solid #222;
-        padding-top: 60px;
-      }
-    `,
-  ],
 })
 export class UiFooterComponent {
+  @Input() margin = 5;
   @Input() centered = false;
   @Input() html: string;
   @Input() text: string;
