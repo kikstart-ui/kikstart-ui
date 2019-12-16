1e28cc8110c74f94790405d68116ad1c8a9b0ae9
Bram Borggreve <borggreve@gmail.com>
2019-12-16 17:27:16 -0500

    fix: prepare for ivy

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -1,13 +1,42 @@
 /*
  * Public API Surface of ui
  */
+export { UiAuthComponent } from './lib/components/ui-auth/ui-auth.component';
+export { UiBrandComponent } from './lib/components/ui-brand.component';
+export { UiCardComponent } from './lib/components/ui-card.component';
+export { UiCardBodyComponent } from './lib/components/ui-card-body.component';
+export { UiCardFooterComponent } from './lib/components/ui-card-footer.component';
+export { UiCardHeaderComponent } from './lib/components/ui-card-header.component';
+export { UiContentComponent } from './lib/components/ui-content.component';
 export { UiDataComponent, UiDataShape } from './lib/components/ui-data.component';
+export { UiDebugComponent } from './lib/components/ui-debug.component';
+export { UiDialogFormComponent } from './lib/components/ui-dialog-form.component';
+export { UiDialogProgressComponent } from './lib/components/ui-dialog-progress.component';
+export { UiDropdownComponent } from './lib/components/ui-dropdown.component';
+export { UiFormComponent } from './lib/components/ui-form.component';
+export { UiFooterComponent } from './lib/components/ui-footer.component';
+export { UiHeaderComponent } from './lib/components/ui-header.component';
+export { UiHeroComponent } from './lib/components/ui-hero.component';
+export { UiLayoutComponent } from './lib/components/ui-layout.component';
+export { UiLinkComponent } from './lib/components/ui-link.component';
+export { UiLinksComponent } from './lib/components/ui-links.component';
+export { UiListComponent } from './lib/components/ui-list.component';
+export { UiListItemComponent } from './lib/components/ui-list-item.component';
+export { UiLoadingComponent } from './lib/components/ui-loading.component';
+export { UiPageSidebarComponent } from './lib/components/ui-page-sidebar.component';
 export { UiPageSidebarRouteComponent } from './lib/components/ui-page-sidebar-route.component';
+export { UiSidebarComponent } from './lib/components/ui-sidebar.component';
+export { UiSidebarItemComponent } from './lib/components/ui-sidebar-item.component';
+export { UiTabsComponent } from './lib/components/ui-tabs.component';
+export { UiUserComponent } from './lib/components/ui-user.component';
+
 export { FormHelper } from './lib/helpers/form.helper';
 export { UiBrand } from './lib/interfaces/ui-brand';
 export { UiCard } from './lib/interfaces/ui-card';
 export { UiLayout } from './lib/interfaces/ui-layout';
 export { UiLink } from './lib/interfaces/ui-link';
 export { UiTab } from './lib/interfaces/ui-tab';
+export { TruncatePipe } from './lib/pipes/truncate.pipe';
 export { UiService } from './lib/services/ui.service';
 export { UiModule } from './lib/ui.module';
+export { UiFormsModule } from './lib/ui-forms.module';
