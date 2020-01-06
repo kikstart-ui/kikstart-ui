539c56632c0ef3542f5ccc26c659f27f6e61bcad
Bram Borggreve <borggreve@gmail.com>
2020-01-06 13:25:32 -0500

    feat: add ui-markdown component

---
+++ src/app/demo/demos/ui-markdown/markdown-simple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <ui-markdown [data]="data"></ui-markdown>
+  `,
+})
+export class MarkdownSimpleComponent {
+  public data = require('!!raw-loader!./markdown-simple.example.md').default as string;
+}
