4cd8f460f53367fe0f0393c7157bd0e9467e754a
Bram Borggreve <borggreve@gmail.com>
2020-01-08 19:21:03 -0500

    feat(ui-markdown): switch to ngx-showdown/showdown.js, add markdown editor demo

--- projects/ui/src/lib/components/ui-markdown.component.ts
+++ projects/ui/src/lib/components/ui-markdown.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit, ViewChild } from '@angular/core'
+import { ShowdownComponent } from 'ngx-showdown'
 
 @Component({
   selector: 'ui-markdown',
   template: `
-    <markdown [data]="data"></markdown>
+    <showdown [value]="data"></showdown>
   `,
 })
-export class UiMarkdownComponent {
+export class UiMarkdownComponent implements OnInit {
+  @ViewChild(ShowdownComponent, { static: true }) showdownComponent: ShowdownComponent
   @Input() public data: string
+
+  ngOnInit() {
+    this.showdownComponent.setOptions({ emoji: true })
+  }
 }
