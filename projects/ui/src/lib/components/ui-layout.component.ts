4150b8014b7790ecff9fc30d33d6d718e1c97021
Derlys <derlys275@gmail.com>
2020-03-25 13:06:56 -0500

    feat: conditionally render header and footer in layout

--- projects/ui/src/lib/components/ui-layout.component.ts
+++ projects/ui/src/lib/components/ui-layout.component.ts
@@ -9,12 +9,13 @@ import { UiLayout } from '../interfaces/ui-layout'
       [links]="config?.header?.links"
       [fluid]="config?.header?.fluid || false"
       [style]="config?.header?.style"
+      *ngIf="config?.brand || config?.header"
     >
     </ui-header>
     <ui-content [fluid]="config?.content?.fluid" [padding]="config?.content?.padding || true">
       <ng-content></ng-content>
     </ui-content>
-    <div class="my-md-5 my-3">
+    <div class="my-md-5 my-3" *ngIf="config?.footer">
       <ui-footer [html]="config?.footer?.html" [links]="config?.footer?.links"></ui-footer>
     </div>
   `,
